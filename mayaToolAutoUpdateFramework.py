85a7924e832033cf5076ca80a05a0fecd520bd62
debugdom <debugdom@e6e1883a-4d57-4d26-b8de-33c250991428>
2009-12-10 00:11:15 +0000

    Initial commandline tool

---
+++ mayaToolAutoUpdateFramework.py
@@ -0,0 +1,38 @@
+from urllib import urlopen
+from xml.dom import minidom
+
+class MayaToolAutoUpdater:
+	""" checks online for updated version from XML file """
+	
+	# current running version of tool
+	currentRunningVersion = 0.0
+	feedPath = ""
+	
+	def setRunningVersion(self, runningVersion):
+		self.currentRunningVersion = runningVersion
+	
+	def setXMLPath(self, xmlPath):
+		self.feedPath = xmlPath
+	
+	def getUpdateInformation(self):
+
+		try:
+		
+			self.updateFeed = urlopen(self.feedPath)
+	
+		except IOError:
+
+			print "IO Error: Cannot to connect to 'tinternet."
+	
+	def parseFeed(self):
+		""" parses the XML feed """
+		domObj = minidom.parseString(self.updateFeed.read())
+		kulerResult = domObj.getElementsByTagName("versionNumber")
+		#bitref = kulerResult[0]
+		print(kulerResult)
+	
+goGetUpdate = MayaToolAutoUpdater()
+goGetUpdate.setRunningVersion(0.9)
+goGetUpdate.setXMLPath("http://update.reality-debug.co.uk/audioAmpExtractor.xml")
+goGetUpdate.getUpdateInformation()
+goGetUpdate.parseFeed()
