d00ee7f91ce2c324d5bdd0e8932694803b176b58
debugdom <debugdom@e6e1883a-4d57-4d26-b8de-33c250991428>
2010-01-11 00:07:20 +0000

    added persistent preference storage system & and lots of other tweaks.

---
+++ mayaAutoUpdateFramework/locationMarker.py
@@ -0,0 +1,3 @@
+# do not delete this file or move it, it is a location marker file to save the preferences (and yes it is a dirty ugly workaround)
+
+#mwahahahaha
--- mayaAutoUpdateFramework/mayaToolAutoUpdateFramework.py
+++ mayaAutoUpdateFramework/mayaToolAutoUpdateFramework.py
@@ -4,80 +4,90 @@ import urllib
 from xml.dom import minidom
 import maya.cmds as cmds
 
+import locationMarker
+
 class MayaUpdateGUI:
 	
 	#class variables
 	
+	
 	def __init__(self, latestVersionNumber, productName, bugFixes, newFeatures, downloadURL):
+		
+		self.preferenceFileName = "mayaUpdatePrefs.prefs"
 		
 		self.fileDownloader = updateDownloadSystem()
 		
 		self.base = downloadURL[downloadURL.rindex('/')+1:]
 		self.downloadURL = downloadURL
 		
-		self.updateInformationString = ("Version: " + latestVersionNumber + "Bug fixes-" + bugFixes + "New features-" + newFeatures)
-		updateInformationWindow = cmds.window(title="There is an update available", iconName='Update information', widthHeight=(200, 55))
+		self.updateInformationString = (productName + "version " + latestVersionNumber + "is now available." + "Bug fixes-" + bugFixes + "New features-" + newFeatures)
+		updateInformationWindow = cmds.window(title="Software Update", iconName='Update', widthHeight=(200, 55))
 		cmds.columnLayout(adjustableColumn=True)
 
 		informationPanel = cmds.scrollField(text=self.updateInformationString, height= 400, width=300, editable=False, wordWrap=True)
 		
+		
+		cmds.checkBox(label='Do not ask again.', onCommand=self.turnOnAutoUpdate, offCommand=self.turnOffAutoUpdate)
+		
 		cmds.button(label='Update', command=self.callDownloadCommand)
-		cmds.button(label='Do not ask again')
+
 		cmds.button(label='Close', command=('cmds.deleteUI(\"' + updateInformationWindow + '\", window=True)'))
 		cmds.setParent('..')
 		cmds.showWindow(updateInformationWindow)
 		
 	def callDownloadCommand(self, *args):
-		#fileDownloader = updateDownloadSystem()
+		
 		self.fileSaveDialog()
 		
-		#self.fileDownloader.geturl(self.downloadURL, ("/moo/" + self.base))
+		self.fileDownloader.geturl(self.downloadURL, self.downloadPath)
 		
 	def fileSaveDialog(self, *args):
-		#print(type(str(self.base)))
+
 		self.downloadPath = cmds.fileDialog(mode=1, directoryMask=str(self.base))
-		
-		#cmds.fileBrowserDialog(m=1, fc=self.callDownloadCommand, actionName='Save File', operationMode='SaveAs', includeName=self.base, filterList=self.base, tipMessage=self.base)
+	
+	def turnOffAutoUpdate(self, *args):
+		defaultPrefContents = "autoUpdateCheck = FALSE"
+			
+		prefFile = open((self.currentLocation() + "/" + self.preferenceFileName),"w")
+		prefFile.write(defaultPrefContents)
+		prefFile.close()
+	
+	def turnOnAutoUpdate(self, *args):
+		defaultPrefContents = "autoUpdateCheck = TRUE"
+			
+		prefFile = open((self.currentLocation() + "/" + self.preferenceFileName),"w")
+		prefFile.write(defaultPrefContents)
+		prefFile.close()
+	
+	def currentLocation(self):
+		currentDir = os.path.dirname(locationMarker.__file__)
+		return currentDir
 		
 class updateDownloadSystem():
 
 	def _reporthook(self, numblocks, blocksize, filesize, url=None):
 
 		fileName = os.path.basename(url)
-		
-		base = ("/moo/" + fileName)
-		
-		#XXX Should handle possible filesize=-1.
-		
-		
+
 		try:
 			percent = min((numblocks*blocksize*100)/filesize, 100)
-			cmds.progressWindow(title='Download', progress=percent*10, status='Downloading: ', isInterruptable=True)
+			cmds.progressWindow(title='Download', progress=percent, status='Downloading: ', isInterruptable=True)
 		except:
 			percent = 100
 			cmds.progressWindow(endProgress=1)
 
 		if numblocks != 0:
-			#sys.stdout.write("\b"*70)
 			cmds.progressWindow(edit=True, progress=percent, status=('Downloading: ' + str(percent) + '%' ))
 		
 		
-		
-		#sys.stdout.write("%-66s%3d%%" % (base, percent))
-
 	def geturl(self, url, dst):
-		#print "get url '%s' to '%s'" % (url, dst)
+		
 		urllib.urlretrieve(url, dst,
 							lambda nb, bs, fs, url=url: self._reporthook(nb,bs,fs,url))
-		#sys.stdout.write('\n')
-
 
 class MayaToolAutoUpdater:
 	""" checks online for updated version from XML file """
-	#todos:
-	# add user called update method
-	
-	# current running version of tool
+
 	currentRunningVersion = ""
 	feedPath = ""
 	latestVersionNumber = ""
@@ -87,10 +97,52 @@ class MayaToolAutoUpdater:
 	productName = ""
 	
 	def __init__(self, productName, currentVersion, xmlPath):
+		
 		self.setRunningVersion(currentVersion)
+		
 		self.setProductName(productName)
+		
 		self.setXMLPath(xmlPath)
+		
+		if(self.checkPreferenceFile()):
+			
+			self.getUpdateInformation()
+		
+		else:
+			pass
+		
+		
+		
+	def checkPreferenceFile(self, *args):
+		preferenceFileName = "mayaUpdatePrefs.prefs"
+		
+		if(os.path.exists(self.currentLocation() + "/" + preferenceFileName)):
+			readPrefs = open((self.currentLocation() + "/" + preferenceFileName),"r")
+			updatePreference = readPrefs.read()
+			readPrefs.close()
+			
+			if(updatePreference.count("autoUpdateCheck = TRUE")):
+				updateStatus = True
+			else:
+				updateStatus = False
+
+			
+		else:
+
+			defaultPrefContents = "autoUpdateCheck = TRUE"
+			
+			prefFile = open((self.currentLocation() + "/" + preferenceFileName),"w")
+			prefFile.write(defaultPrefContents)
+			prefFile.close()
+			
+		return updateStatus
 	
+	def userQueriesUpdate(self):
+		if (str(self.currentRunningVersion.strip()) != str(self.latestVersionNumber.strip())):
+			self.mayaGUI = MayaUpdateGUI(self.latestVersionNumber, self.productName, self.bugFixList, self.newFeaturesList, self.newFileURL)
+		else:
+			cmds.confirmDialog(title="wah wah wah", message="there is no update available.",button="*sighs*")
+
 	def setRunningVersion(self, runningVersion):
 		""" sets the current running version of the script """
 		self.currentRunningVersion = runningVersion
@@ -113,9 +165,14 @@ class MayaToolAutoUpdater:
 		except IOError:
 
 			print "IO Error: Cannot to connect to 'tinternet."
+
+	def currentLocation(self):
+		currentDir = os.path.dirname(locationMarker.__file__)
+		return currentDir
 	
 	def parseFeed(self):
 		""" parses the XML feed """
+		
 		domObj = minidom.parseString(self.updateFeed.read())
 		versionNumberResult = domObj.getElementsByTagName("versionNumber")
 		bitref = versionNumberResult[0]
@@ -134,9 +191,11 @@ class MayaToolAutoUpdater:
 		self.newFileURL = bitref.childNodes[0].nodeValue
 		
 		if (str(self.currentRunningVersion.strip()) != str(self.latestVersionNumber.strip())):
-			mayaGUI = MayaUpdateGUI(self.latestVersionNumber, self.productName, self.bugFixList, self.newFeaturesList, self.newFileURL)
+			self.mayaGUI = MayaUpdateGUI(self.latestVersionNumber, self.productName, self.bugFixList, self.newFeaturesList, self.newFileURL)
 			
-goGetUpdate = MayaToolAutoUpdater("AudioAmpExtractor", "0.8a", "http://update.reality-debug.co.uk/audioAmpExtractor.xml")
 
-goGetUpdate.getUpdateInformation()
-#goGetUpdate.parseFeed()
+
+
+#goGetUpdate = MayaToolAutoUpdater("AudioAmpExtractor", "0.8a", "http://update.reality-debug.co.uk/audioAmpExtractor.xml")
+
+#goGetUpdate.userQueriesUpdate()
